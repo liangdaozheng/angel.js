0a33bd9fc9bae5d85fff51848e1e4c482fc9215f
liangdaozheng <l_daozheng@163.com>
2020-12-24 18:56:51 +0800

    Add files via upload

---
+++ angel.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * angel v1.0.0 
+ * Copyright 2020
+ *
+ * By angel FE Team
+ * 
+ */
+//start
+class Angel {
+  /**
+   * Each method is labeled with the author's abbreviation
+   */
+  //author Mike.L
+
+  
+}//end
